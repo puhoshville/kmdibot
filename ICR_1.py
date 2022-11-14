d9d249077fc73df0a5ddbdcf8b74802993459154
Dmitrii Pukhov <pukhovdn@gmail.com>
2022-11-14 11:10:24 +0400

    Update tesseract config
    --psm 7 (Treat the image as a single text line.)
    --oem 1 (Neural nets LSTM engine only.)

--- ICR_1.py
+++ ICR_1.py
@@ -6,7 +6,7 @@ import os
 def get_captcha(img_path):
     captcha = pytesseract.image_to_string(
         Image.open(img_path),
-        config='--psm 8 -c tessedit_char_whitelist=0123456789'
+        config='--psm 7 --oem 1 -c tessedit_char_whitelist=0123456789'
         )
     captcha = captcha.replace(" ", "").strip()
     print("captcha", captcha)
