a70c85508a5ee3a94a1b30401323ccc5152c0e0a
superkoti <yola@live.ru>
2023-01-19 13:34:25 +0400

    small update

--- write_logs.py
+++ write_logs.py
@@ -10,18 +10,12 @@ def wl(status, text):
             "\t" + text + "\n"
         )
 
-    if count_lines("logs") == 15000:
+    if len(f.readlines()) == 15000:
         f = open("logs").readlines()
         for i in range(5000):
             f.pop(i)
         with open("logs", 'w') as F:
             F.writelines(f)
-
-
-def count_lines(filename, chunk_size=1 << 13):
-    with open(filename) as file:
-        return sum(chunk.count('\n')
-                   for chunk in iter(lambda: file.read(chunk_size), ''))
 
 
 def form_st(not_a_string):
